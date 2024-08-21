57ff380a1556caca4f3230a85957807a137b9772
sallyibrahim117 <sallyibrahim117@gmail.com>
2024-08-21 02:57:22 -0700

    fix

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const items = [
   {
+    id:1,
     img: "imaages/2.jpg",
     type: 'waffele',
     tittle: 'waffle with berries',
     salary: 6
   },
 
-  {
+{
+    id:2,
     img: "imaages/1.jpg",
     type: 'waffele',
     tittle: 'cheese cake',
@@ -14,6 +16,7 @@ const items = [
   },
 
   {
+    id:3,
     img: "imaages/3.jpg",
     type: 'waffele',
     tittle: 'white cake',
@@ -22,6 +25,7 @@ const items = [
   },
 
   {
+    id:4,
     img: "imaages/4.jpg",
     type: 'waffele',
     tittle: 'waffle with gatoo',
@@ -29,6 +33,7 @@ const items = [
 
   },
   {
+    id:5,
     img: "imaages/5.jpg",
     type: 'waffele',
     tittle: 'waffle with berries',
@@ -36,13 +41,17 @@ const items = [
 
   },
   {
+    id:6,
     img: "imaages/6.jpg",
     type: 'waffele',
     tittle: 'waffle with berries',
     salary: 16
 
   },
-  {img: "imaages/8.jpg",
+
+  {
+    id:7,
+    img: "imaages/8.jpg",
     type: 'waffele',
     tittle: 'waffle with berries',
     salary: 6
@@ -50,6 +59,7 @@ const items = [
   }
   ,
   {
+    id:8,
     img: "imaages/9.jpg",
     type: 'waffele',
     tittle: ' berries',
@@ -58,6 +68,7 @@ const items = [
   }
   ,
   {
+    id:9,
     img: "imaages/10.jpg",
     type: 'waffele',
     tittle: 'cheese with berries',
@@ -77,17 +88,27 @@ items.forEach(element => {
               <img src="${element.img}" class="dessert-1" alt="">
             </div>
             <div class="button-info">
-              <button onclick='addToCart(${JSON.stringify(element)})'>
-                <span>
-                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-garden-cart" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d53c3c" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M17.5 17.5m-2.5 0a2.5 2.5 0 1 0 5 0a2.5 2.5 0 1 0 -5 0" />
-                    <path d="M6 8v11a1 1 0 0 0 1.806 .591l3.694 -5.091v.055" />
-                    <path d="M6 8h15l-3.5 7l-7.1 -.747a4 4 0 0 1 -3.296 -2.493l-2.853 -7.13a1 1 0 0 0 -.928 -.63h-1.323" />
-                  </svg>
-                </span>
-                <span>Add To Cart</span>
+            <div>
+              <button onclick='addToCart(${JSON.stringify(element)},this)'>
+            
+              <span>
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-garden-cart" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d53c3c" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M17.5 17.5m-2.5 0a2.5 2.5 0 1 0 5 0a2.5 2.5 0 1 0 -5 0" />
+                <path d="M6 8v11a1 1 0 0 0 1.806 .591l3.694 -5.091v.055" />
+                <path d="M6 8h15l-3.5 7l-7.1 -.747a4 4 0 0 1 -3.296 -2.493l-2.853 -7.13a1 1 0 0 0 -.928 -.63h-1.323" />
+              </svg>
+            </span>
+            <span>Add To Cart</span>
+           
               </button>
+              <button class="second">
+              <i class="fa-solid fa-circle-minus"></i>
+              <span>1</span>
+              <i class="fa-solid fa-circle-plus"></i>
+              </button>
+              </div>
+              
             </div>
           </div>
 
@@ -104,36 +125,56 @@ var appendItems=document.querySelector('.appendItems');
 var cartCounter=document.querySelector('.cart-counter');
 var compute=document.querySelector('.compute');
 var totalSalary=document.querySelector('.total-salary');
-function addToCart(el) {
-  var totalPrice=0;
-   var noCardContent=document.querySelector('.noCartContent');
+var noCardContent=document.querySelector('.noCartContent');
+var totalPrice=0;
+function addToCart(el , btn) {
  cartItems.push(el);
  console.log(cartItems);
  cartCounter.innerHTML=cartItems.length;
 //  cartItems.unshift(el);
- var newCart=document.createElement('div');
-  newCart.innerHTML=`      <div class="d-flex space-between justify-content-center b-bottom p-5">
-  <span>
-      <h6>${el.tittle}</h6>
-  <p class="text-gray mt-1">${el.salary}</p>
-  </span>
-  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-      <path d="M10 10l4 4m0 -4l-4 4" />
-    </svg>
+console.log(cartItems);
+    var newCart=document.createElement('div');
+     newCart.innerHTML=`<div id='cart-id-${el.id} , this' class="d-flex space-between justify-content-center b-bottom p-5">
+     <span>
+         <h6>${el.tittle}</h6>
+     <p class="text-gray mt-1">${el.salary}</p>
+     </span>
+     <a href="#" onclick="clearCart(${el.id} , this)">
+     <svg  xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+     <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+     <path d="M10 10l4 4m0 -4l-4 4" />
+   </svg></a>
+    
+   
+   </div> `;
+    appendItems.appendChild(newCart);
 
-</div> `;
- appendItems.appendChild(newCart);
 if(cartItems.length > 0){
   noCardContent.style.display='none';
   compute.style.display='block';
 }
+totalPrice=0;
 cartItems.forEach(element => {//{img,salary,title}
   console.log(element.salary);
   totalPrice+=element.salary;
   console.log(totalPrice);
 });
 totalSalary.innerHTML=totalPrice;
-};
 
+btn.style.display='none';
+btn.nextElementSibling.style.display='block';
+
+}
+
+
+function clearCart(id , val){
+  
+cartItems.forEach((element,index) => {
+  if (id == element.id){
+    cartItems.splice(index , 1);
+    totalSalary.innerHTML=totalPrice - element.salary;
+  }
+});
+val.parentElement.style.display="none";
+}
